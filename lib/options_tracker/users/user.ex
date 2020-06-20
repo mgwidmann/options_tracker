de215b24384760caa29429924e4f011bbecd1008
matt <mgwidmann@gmail.com>
2020-06-20 01:26:52 -0400

    Generate live users resource

---
+++ lib/options_tracker/users/user.ex
@@ -0,0 +1,18 @@
+defmodule OptionsTracker.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :username])
+    |> validate_required([:email, :username])
+  end
+end
