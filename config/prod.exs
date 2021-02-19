956da481efd1a14f2a135a014060ecbe9f1bd68d
matt <mgwidmann@gmail.com>
2021-02-19 01:04:10 -0500

    Ensure links are generated as https not http.

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :options_tracker, OptionsTrackerWeb.Endpoint,
-  url: [host: "options-tracker.gigalixirapp.com", port: 443],
+  url: [host: "options-tracker.gigalixirapp.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
